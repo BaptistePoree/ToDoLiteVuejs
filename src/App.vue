e141dddec7e43b702d605a5b4326bfb139f52b0e
BaptistePoree <baptiste.poree@gmail.com>
2021-03-18 14:40:44 +0100

    code de WANG Yibo (probleme de git)

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <todoliste > </todoliste>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
+  
 </template>
-
-<script>
-import todoliste from './components/toDoListe.vue'
-
-export default {
-  name: 'App',
-  newTodo: ' ' ,
-  filter : 'all',
-  components: 
-  {
-    todoliste
-  },
-  data()
-  {
-    todoliste[
-      {
-        id: 1,
-        name : 'tache 1',
-        completed : false
-      },
-      {
-        id: 1,
-        name : 'tache 1',
-        completed : false
-      }
-    ]
-    
-  },
-}
-</script>
-
-
-
